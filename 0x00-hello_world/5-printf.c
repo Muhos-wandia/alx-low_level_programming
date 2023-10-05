3560879a15c4f5b637fd3a01215f74e1ae5c39e5
Muhos-wandia <muhorowandia@gmail.com>
2023-10-05 12:07:04 -0700

    Task 6

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 when successful
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
